3bb641bb0844c79431c7e149a2967685c950c657
Tim Holy <holy @nospam@ wustl.edu>
2009-04-23 09:32:22 +0000

    First content for the "for developers" page. This is also simply a test to see if this works or not.
    git-svn-id: svn://svn.debian.org/svn/tux4kids/tux4kids-web@955 47d30e19-490b-0410-9d18-e851f4e465b3

--- htdocs/newsitetest/tux4kids/dev.php
+++ htdocs/newsitetest/tux4kids/dev.php
@@ -18,7 +18,14 @@ echo'
 				</ul>
 			</div>
 			<div class="col2">
-			This one is for you Tim Holy, I would not know what sort of information to put up here.
+			<b>General information</b>
+			Tux4Kids programs are written in C and use Simple DirectMedia Layer (SDL) for the graphics. The programs are portable, and run on Linux, Windows, Mac OSX, and BeOS. Most development happens on Linux, and the tools for development are readily available on any modern Linux distribution. However, it should also be possible to develop on other platforms.<br />
+			<br />
+			Here are the major development tools and libraries used by Tux4Kids programs:<br />
+			<a href="http://www.libsdl.org/">SDL</a> (see in particular the DocWiki link)<br />
+			<font style="font-size: 10px;">We also use SDL_image (graphics files), SDL_mixer (sound), SDL_ttf (fonts), and will soon add SDL_net (network). Documentation for these is available on the DocWiki.</font><br /> 
+			<a href="http://svnbook.red-bean.com/">Subversion</a> (see in particular Chapter 2, "Basic Usage")<br />
+			
 			</div>
 		</div>
 	</div>
