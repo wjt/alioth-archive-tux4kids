74840e91ad0890368bbda1b38032a7390f8e7c5c
Matthew McSpadden <webmaster @nospam@ anviltech.com>
2009-05-20 17:59:50 +0000

    Changed the downloads page on Tuxmath.
    git-svn-id: svn://svn.debian.org/svn/tux4kids/tux4kids-web@980 47d30e19-490b-0410-9d18-e851f4e465b3

--- htdocs/newsitetest/tuxmath/download.php
+++ htdocs/newsitetest/tuxmath/download.php
@@ -18,36 +18,217 @@ echo'
 				</ul>
 			</div>
 			<div class="col2">
-				<a href="sourcedown.php"><strong>Source</strong></a><br />
-				<img src="images/gear.gif" alt="Gears" />Get the source and compile it yourself.<br />
-				Clicking the link above would take you to yet<br />
-				another page where the download links can <br />
-				be found. 
-				<br />
-				<br />
-				<br />
-				<a href="windown.php"><strong>Windows 2000, XP, Vista</strong></a><br />
-				<img src="images/windows.gif" alt="Windows Logo" />Here is a blurb about Windows Users<br />
-				Clicking the link above would take you to yet<br />
-				another page where the download links can <br />
-				be found. 
-				<br />
-				<br />
-				<br />
-				<a href="macdown.php"><strong>Apple Mac OS X</strong></a><br />
-				<img src="images/macosx.gif" alt="Mac OS X Logo" />Here is a blurb about Mac Users<br />
-				Clicking the link above would take you to yet<br />
-				another page where the download links can <br />
-				be found. 
-				<br />
-				<br />
-				<br />
-				<a href="debdown.php"><strong>Ubuntu Linux</strong></a><br />
-				<img src="images/linux-ubuntu.gif" alt="Ubuntu Logo" />Here is a blurb about Linux Users<br />
-				Clicking the link above would take you to yet<br />
-				another page where the download links can <br />
-				be found. 
-				<br />
+				<table class="downloads">
+					<tr>
+						<th>
+							Source
+						</th>
+						<th>
+							Windows
+						</th>
+						<th>
+							Mac
+						</th>
+						<th>
+							Debian Linux
+						</th>
+					</tr>
+					<tr>
+						<th>
+							<a href="file.tar.gz">(1.7.2)</a>
+						</th>
+						<th>
+							<a href="file.tar.gz">(1.7.2)</a>
+						</th>
+						<th>
+							<a href="file.tar.gz">(1.7.2)</a>
+						</th>
+						<th>
+							<a href="file.tar.gz">How to</a>
+						</th>
+					</tr>
+					<tr>
+						<td>
+							<a href="file.tar.gz">1.7.1</a>
+						</td>
+						<td>
+							<a href="file.tar.gz">1.7.1</a>
+						</td>
+						<td>
+							<a href="file.tar.gz">1.7.1</a>
+						</td>
+						<td>
+							&nbsp;
+						</td>
+					</tr>
+					<tr style="background-color: #fff">
+						<td>
+							<a href="file.tar.gz">1.7.0</a>
+						</td>
+						<td>
+							<a href="file.tar.gz">1.7.0</a>
+						</td>
+						<td>
+							&nbsp;
+						</td>
+						<td>
+							&nbsp;
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<a href="file.tar.gz">1.6.3</a>
+						</td>
+						<td>
+							<a href="file.tar.gz">1.6.3</a>
+						</td>
+						<td>
+							&nbsp;
+						</td>
+						<td>
+							&nbsp;
+						</td>
+					</tr>
+					<tr style="background-color: #fff">
+						<td>
+							<a href="file.tar.gz">1.6.2</a>
+						</td>
+						<td>
+							<a href="file.tar.gz">1.6.2</a>
+						</td>
+						<td>
+							&nbsp;
+						</td>
+						<td>
+							&nbsp;
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<a href="file.tar.gz">1.6.1</a>
+						</td>
+						<td>
+							<a href="file.tar.gz">1.6.1</a>
+						<td>
+							&nbsp;
+						</td>
+						<td>
+							&nbsp;
+						</td>
+					</tr>
+					<tr style="background-color: #fff">
+						<td>
+							<a href="file.tar.gz">1.6.0</a>
+						</td>
+						<td>
+							<a href="file.tar.gz">1.6.0</a>
+						</td>
+						<td>
+							&nbsp;
+						</td>
+						<td>
+							&nbsp;
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<a href="file.tar.gz">1.5.9</a>
+						</td>
+						<td>
+							<a href="file.tar.gz">1.5.6</a>
+						</td>
+						<td>
+							&nbsp;
+						</td>
+						<td>
+							&nbsp;
+						</td>
+					</tr>
+					<tr style="background-color: #fff">
+						<td>
+							<a href="file.tar.gz">1.5.8</a>
+						</td>
+						<td>
+							<a href="file.tar.gz">1.5.5</a>
+						</td>
+						<td>
+							&nbsp;
+						</td>
+						<td>
+							&nbsp;
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<a href="file.tar.gz">1.5.7</a>
+						</td>
+						<td>
+							&nbsp;
+						</td>
+						<td>
+							&nbsp;
+						</td>
+						<td>
+							&nbsp;
+						</td>
+					</tr>
+					<tr style="background-color: #fff">
+						<td>
+							<a href="file.tar.gz">1.5.6</a>
+						</td>
+						<td>
+							&nbsp;
+						</td>
+						<td>
+							&nbsp;
+						</td>
+						<td>
+							&nbsp;
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<a href="file.tar.gz">1.5.5</a>
+						</td>
+						<td>
+							&nbsp;
+						</td>
+						<td>
+							&nbsp;
+						</td>
+						<td>
+							&nbsp;
+						</td>
+					</tr>
+					<tr style="background-color: #fff">
+						<td>
+							<a href="file.tar.gz">1.5.4</a>
+						</td>
+						<td>
+							&nbsp;
+						</td>
+						<td>
+							&nbsp;
+						</td>
+						<td>
+							&nbsp;
+						</td>
+					</tr>
+					<tr>
+						<td>
+							<a href="file.tar.gz">1.5.3</a>
+						</td>
+						<td>
+							&nbsp;
+						</td>
+						<td>
+							&nbsp;
+						</td>
+						<td>
+							&nbsp;
+						</td>
+					</tr>
+				</table>
 			</div>
 		</div>
 	</div>
