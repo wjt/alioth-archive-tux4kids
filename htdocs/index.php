f447a09292e7851c6dbc3941c7930f07e6a6d013
Matthew McSpadden <webmaster @nospam@ anviltech.com>
2009-06-14 02:50:22 +0000

    Fixed the forum link which linked to a non secure page. Turned the tux4kids logo at the top into a link to the homepage. Finished the mailing list page. Modified the menu to direct to the mailing list page that includes all the mailing lists.
    git-svn-id: svn://svn.debian.org/svn/tux4kids/tux4kids-web@1045 47d30e19-490b-0410-9d18-e851f4e465b3

--- htdocs/index.php
+++ htdocs/index.php
@@ -8,12 +8,12 @@ echo'
 		<div class="box">
 			<div class="col1">
 				<ul class="menu">
-					<li><a href="" class="active"><span>Home</span></a></li>
+					<li><a href="index.php" class="active"><span>Home</span></a></li>
 					<li><a href="projects.php"><span>Projects</span></a></li>
 					<li><a href="dev.php"><span>For Developers</span></a></li>
-					<li><a href="http://alioth.debian.org/mail/?group_id=31080"><span>Mailing Lists</span></a></li>
+					<li><a href="mail.php"><span>Mailing Lists</span></a></li>
 					<li><a href="svn.php"><span>SVN Repository</span></a></li>
-					<li><a href="http://alioth.debian.org/forum/?group_id=31080"><span>Forums</span></a></li>
+					<li><a href="https://alioth.debian.org/forum/?group_id=31080"><span>Forums</span></a></li>
 				</ul>
 				<br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
 				<br /><br /><br /><br /><br /><br /><br /><br />
