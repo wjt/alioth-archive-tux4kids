cff12070b47b884d059e6687e34716f4c916e94e
Matthew McSpadden <webmaster @nospam@ anviltech.com>
2009-06-27 16:21:37 +0000

    Continued corrections for the svn website documents.
    git-svn-id: svn://svn.debian.org/svn/tux4kids/tux4kids-web@1099 47d30e19-490b-0410-9d18-e851f4e465b3

--- htdocs/newsitetest/tux4kids/includes/header.php
+++
@@ -1,5 +0,0 @@
-			<div class="header">
-			<span class="drop"><img src="images/title.png" alt="Tux4Kids logo" /><br />
-			High Quality Educational Software Alternatives
-			<span><img src="images/tux.png" alt="Tux" /></span></span>
-			</div>
