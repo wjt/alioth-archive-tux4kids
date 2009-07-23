5c8e4073be09a247ce9b1df7bde8e9d3c2131950
Matthew McSpadden <webmaster @nospam@ anviltech.com>
2009-07-23 19:04:42 +0000

    working to fix svn and IE6 issues
    git-svn-id: svn://svn.debian.org/svn/tux4kids/tux4kids-web@1259 47d30e19-490b-0410-9d18-e851f4e465b3

---
+++ htdocs/newsitetest/tux4kids/includes/header.php
@@ -0,0 +1,5 @@
+			<div class="header">
+			<span class="drop"><a href="http://tux4kids.alioth.debian.org/"><img src="images/title.png" alt="Tux4Kids logo" /></a><br />
+			High Quality Educational Software Alternatives
+			<span><img src="images/tux.png" alt="Tux" /></span></span>
+			</div>
