2b40ed5d4c7e9ed95489a38bc195b9fb0cc83c68
David Bruce <davidstuartbruce@gmail.com>
2010-02-03 12:45:50 -0600

    Use new tux_family image

--- htdocs/includes/header.php
+++ htdocs/includes/header.php
@@ -1,18 +1,12 @@
 			<div class="headr">
 				<table>
 					<tr>
-						<td style="width: 70%;">
+						<td style="width: 90%;">
 							<a href="http://tux4kids.alioth.debian.org/"><img src="images/title.png" alt="Tux4Kids logo" /></a><br />
 							High Quality Educational Software Alternatives
 						</td>
 						<td style="width: 10%;">
-							<img src="images/BabyTuxAlpha_scaled.png" alt="BabyTux" />
-						</td>
-						<td style="width: 10%;">
-							<img src="images/BabyTuxSitAlpha_scaled.png" alt="BabyTuxSit" />
-						</td>
-						<td style="width: 10%;">
-							<img src="images/tux.png" alt="Tux" />
+							<img src="images/tux_family.png" alt="Tux_Family" />
 						</td>
 					</tr>
 				</table>
