74c0b8de523edaa2c8dd453740d996eca343466c
Bastien <bzg@gnu.org>
2016-04-16 09:12:01 +0200

    Change indentation.
    (This is a test commit.)

--- htdocs/git.php
+++ htdocs/git.php
@@ -1,72 +1,71 @@
 <?php include ('includes/pageheader.php');?>
-	<title>Tux4kids - Git</title>
-	</head>
+<title>Tux4kids - Git</title>
+</head>
 <body>
-	<div align="center">
-		<div class="box">
-			<div class="col1">
-				<ul class="menu">
-					<li><a href="index.php"><span>Home</span></a></li>
-					<li><a href="tuxmath.php"><span>TuxMath</span></a></li>
-					<li><a href="tuxpaint.php"><span>TuxPaint</span></a></li>
-					<li><a href="tuxtyping.php"><span>TuxTyping</span></a></li>
-					<li><a href="dev.php"><span>For Developers</span></a></li>
-					<li><a href="mail.php"><span>Mailing Lists</span></a></li>
-					<li><a href="git.php" class="active"><span>Git Repository</span></a></li>
-					<li><a href="https://alioth.debian.org/forum/?group_id=31080"><span>Forums</span></a></li>
-				</ul>
-				<br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-				<br /><br /><br /><br /><br />
-				<img src="images/pow-fusionforge.png" alt="Fusion Forge" />
-			</div>
-			<div class="col2">
-				Tux4kids has now officially switched from Subversion to Git. The old monolithic 
-				"tux4kids" svn repository has been replaced by six git repositories, one for 
-				each subproject, located at git.debian.org/git/tux4kids/*:<br /><br />
+  <div align="center">
+    <div class="box">
+      <div class="col1">
+	<ul class="menu">
+	  <li><a href="index.php"><span>Home</span></a></li>
+	  <li><a href="tuxmath.php"><span>TuxMath</span></a></li>
+	  <li><a href="tuxpaint.php"><span>TuxPaint</span></a></li>
+	  <li><a href="tuxtyping.php"><span>TuxTyping</span></a></li>
+	  <li><a href="dev.php"><span>For Developers</span></a></li>
+	  <li><a href="mail.php"><span>Mailing Lists</span></a></li>
+	  <li><a href="git.php" class="active"><span>Git Repository</span></a></li>
+	  <li><a href="https://alioth.debian.org/forum/?group_id=31080"><span>Forums</span></a></li>
+	</ul>
+	<br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+	<br /><br /><br /><br /><br />
+	<img src="images/pow-fusionforge.png" alt="Fusion Forge" />
+      </div>
+      <div class="col2">
+	Tux4kids has now officially switched from Subversion to Git. The old monolithic 
+	"tux4kids" svn repository has been replaced by six git repositories, one for 
+	each subproject, located at git.debian.org/git/tux4kids/*:<br /><br />
 
-				<br /><b>Browsable Links:</b><br /><br />
+	<br /><b>Browsable Links:</b><br /><br />
 
-				<li><a href="http://git.debian.org/?p=tux4kids/tuxmath.git">http://git.debian.org/?p=tux4kids/tuxmath.git</a></li>
-				<li><a href="http://git.debian.org/?p=tux4kids/tuxtype.git">http://git.debian.org/?p=tux4kids/tuxtype.git</a></li>
-				<li><a href="http://git.debian.org/?p=tux4kids/tuxhistory.git">http://git.debian.org/?p=tux4kids/tuxhistory.git</a></li>
-				<li><a href="http://git.debian.org/?p=tux4kids/tux4kids-web.git">http://git.debian.org/?p=tux4kids/tux4kids-web.git</a></li>
-				<li><a href="http://git.debian.org/?p=tux4kids/tux4kids-admin.git">http://git.debian.org/?p=tux4kids/tux4kids-admin.git</a></li>
-				<li><a href="http://git.debian.org/?p=tux4kids/t4kcommon.git">http://git.debian.org/?p=tux4kids/t4kcommon.git</a></li>
+	<li><a href="http://git.debian.org/?p=tux4kids/tuxmath.git">http://git.debian.org/?p=tux4kids/tuxmath.git</a></li>
+	<li><a href="http://git.debian.org/?p=tux4kids/tuxtype.git">http://git.debian.org/?p=tux4kids/tuxtype.git</a></li>
+	<li><a href="http://git.debian.org/?p=tux4kids/tuxhistory.git">http://git.debian.org/?p=tux4kids/tuxhistory.git</a></li>
+	<li><a href="http://git.debian.org/?p=tux4kids/tux4kids-web.git">http://git.debian.org/?p=tux4kids/tux4kids-web.git</a></li>
+	<li><a href="http://git.debian.org/?p=tux4kids/tux4kids-admin.git">http://git.debian.org/?p=tux4kids/tux4kids-admin.git</a></li>
+	<li><a href="http://git.debian.org/?p=tux4kids/t4kcommon.git">http://git.debian.org/?p=tux4kids/t4kcommon.git</a></li>
 
- 
-				<br /><br /><b>Cloning the Repositories:</b><br /><br />
-				1. Push-enabled ("read/write") cloning for members of the Tux4Kids project on Alioth: (obviously replacing YOUR_ALIOTH_USERID with your actual login, and REPO.git with the name of the desired repository). 
+	
+	<br /><br /><b>Cloning the Repositories:</b><br /><br />
+	1. Push-enabled ("read/write") cloning for members of the Tux4Kids project on Alioth: (obviously replacing YOUR_ALIOTH_USERID with your actual login, and REPO.git with the name of the desired repository). 
 
-				<br />	<br /><kbd>git clone git+ssh://YOUR_ALIOTH_USERID@git.debian.org/git/tux4kids/REPO.git</kbd>
- 
-				<br /><br />You will be prompted for your alioth password each time you use the repository.
- 
-				<br /><br />2. Pull-only ("read-only") cloning:<br />
- 
-				<br /><kbd>git clone git://git.debian.org/git/tux4kids/REPO.git</kbd><br /><br />
+	<br />	<br /><kbd>git clone git+ssh://YOUR_ALIOTH_USERID@git.debian.org/git/tux4kids/REPO.git</kbd>
+	
+	<br /><br />You will be prompted for your alioth password each time you use the repository.
+	
+	<br /><br />2. Pull-only ("read-only") cloning:<br />
+	
+	<br /><kbd>git clone git://git.debian.org/git/tux4kids/REPO.git</kbd><br /><br />
 
-				or:
- 
-				<br /><br /><kbd>git clone http://git.debian.org/git/tux4kids/REPO.git</kbd><br /><br />
- 
+	or:
+	
+	<br /><br /><kbd>git clone http://git.debian.org/git/tux4kids/REPO.git</kbd><br /><br />
+	
+	Note that the autogenerated documentation under the SCM tab at 
+	<a href="https://alioth.debian.org/projects/tux4kids">https://alioth.debian.org/projects/tux4kids</a></li>
+assumes a single global "tux4kids" 
+repository, and thus does not work as described there.  Hopefully this will be 
+corrected in the near future.
 
-				Note that the autogenerated documentation under the SCM tab at 
-				<a href="https://alioth.debian.org/projects/tux4kids">https://alioth.debian.org/projects/tux4kids</a></li>
-				assumes a single global "tux4kids" 
-				repository, and thus does not work as described there.  Hopefully this will be 
-				corrected in the near future.
- 
-				There are two remaining known issues with the migration to git.  Namely, there 
-				are a couple of areas from the old repository that are not included in the six 
-				new git repositories, namely "tuxmath/people", 
-				"branches/commonification/tuxmath", and "branches/commonification/tuxtype".
-				
-				<br /><br />To become a member of Tux4kids, <a href="http://alioth.debian.org">apply for an Alioth account</a> and request to be added to our project. Once you
-				have approval, you can clone any of the repositories as described in #1 above, and will have privileges to push changes to the server. 
-				<br />
-			</div>
+There are two remaining known issues with the migration to git.  Namely, there 
+are a couple of areas from the old repository that are not included in the six 
+new git repositories, namely "tuxmath/people", 
+"branches/commonification/tuxmath", and "branches/commonification/tuxtype".
+
+<br /><br />To become a member of Tux4kids, <a href="http://alioth.debian.org">apply for an Alioth account</a> and request to be added to our project. Once you
+have approval, you can clone any of the repositories as described in #1 above, and will have privileges to push changes to the server. 
+<br />
+</div>
 <?php include ('includes/header.php');?>
-		</div>
-	</div>
+</div>
+</div>
 </body>
 </html>
